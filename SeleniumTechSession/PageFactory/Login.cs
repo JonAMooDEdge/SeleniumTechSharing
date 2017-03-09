b32e902e88a993eed9438e88540691b627af4127
JonAMooDEdge <jonathan.alsop@moodinternational.com>
2017-03-09 16:27:48 +0000

    Added pagefactory for login

---
+++ SeleniumTechSession/PageFactory/Login.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTechSession.PageFactory
+{
+    public class Login : PageFactory
+    {
+        public Login(IWebDriver driver) : base(driver) { }
+
+        [FindsBy(How=How.Id, Using= "UserName")]
+        public IWebElement Username { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "btn")]
+        public IWebElement LoginButton { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "card-content")]
+        public IWebElement ErrorMessage { get; set; }
+    }
+}
